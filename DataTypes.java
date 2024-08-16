970620f1854b710bd57881185419eee3f6d95ee9
Saranya241996 <vssaranya2404@gmail.com>
2024-08-16 12:45:16 +0300

    Initial Commit

---
+++ DataTypes.java
@@ -0,0 +1,60 @@
+package JavaBasics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		//1.integer, it takes only the numerical values not an decimal value
+		
+		int i=50;
+		
+		int j=30;
+		
+	        i=20;
+	        
+	        System.out.println(i+j);
+	        
+	    //Double. it takes decimal values.
+	   /* Note: We can store integer value in double, but we can't store double value in integer*/
+	        
+	    double d=23.33;
+	    
+	    double d1=100;
+	    
+	    double d2=5;
+	    
+	    System.out.println(d1);
+	   
+	    
+	    //character, it takes alphabets, which is in single quotes ''
+	   
+	    /* Note: If we are giving an integer or double value in single quotes, it will be consider as character 
+	     (Note it should be only one character like 'a','1' not as more than one value example: 'ab', '100')*/
+	   
+	  
+	   char c='a';
+	   
+	   char c1='1';
+	   
+	   System.out.println(c1);
+	   
+	   //boolean, it only takes true or false.
+	   
+	   boolean b=true;
+	   
+	   boolean b1= false;
+	   
+	   System.out.println(b1);
+	   
+	   //String, string is not a data type , it is a class. which should be in double quotes "". String S is capital letter.
+	   
+	   String s1="Hi , this is my first java code";
+	   
+	   String s2="Hello World";
+	   
+	   System.out.println(s2);
+	}
+
+}
